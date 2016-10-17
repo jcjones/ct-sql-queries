6eca5478258cfa45836cbee3660c26ab918cbf1b
J.C. Jones <james.jc.jones@gmail.com>
2016-10-17 15:04:49 +0000

    Add telemetry %PageLoadsTLS to Statistics-LEGraph

--- Maintenance-Nightly/Statistics-LEGraph.sql
+++ Maintenance-Nightly/Statistics-LEGraph.sql
@@ -1,1 +1,4 @@
-SELECT * FROM `ctdb`.`le_timeline`;
+SELECT `le_t`.*, (`ff`.`countTLS` / `ff`.`countPageloads`) * 100 AS `percentPageloadsAreTLS`
+  FROM `ctdb`.`le_timeline` AS le_t
+  LEFT JOIN `ctdb`.`firefoxpageloadstls` AS ff
+  ON `ff`.`datestamp`=`le_t`.`datestamp`;
