b63131473fbc93a0443a651c055d7744e873d7db
J.C. Jones <james.jc.jones@gmail.com>
2016-10-21 01:35:26 +0000

    Use 3-letter ISO country codes for countries

--- Maintenance-Nightly/Statistics-LETimelineCounties.sql
+++ Maintenance-Nightly/Statistics-LETimelineCounties.sql
@@ -1,2 +1,9 @@
-SELECT datestamp, country, numCertsActive AS numFQDNsActive
-   FROM  le_timeline_countries;
+SELECT
+    `t`.`datestamp`,
+    `cc`.`ISO3166-1-Alpha-3`,
+    `t`.`numCertsActive` AS `numFQDNsActive`
+FROM
+    `le_timeline_countries` AS `t`
+        JOIN
+    `countrycodes` AS `cc` ON `t`.`country` = `cc`.`ISO3166-1-Alpha-2`
+ORDER BY `datestamp` DESC, `numFQDNsActive` DESC;
